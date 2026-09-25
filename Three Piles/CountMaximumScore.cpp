391a1e6ba218052ee8d9a01c8da6ec8c7e022793
Oalid Khan <oalidkhan69@gmail.com>
2026-09-25 23:44:40 +0600

    Refactor score calculation in CountMaximumScore.cpp

---
+++ Three Piles/CountMaximumScore.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+        cout << max(abs(a - b), abs(a + c - b)) << '\n';
+    }
+    return 0;
+}
